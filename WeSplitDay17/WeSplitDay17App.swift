722bcd1f5cac0e844062936b7eaff4382ff7c80c
mikoongcoding <skymikong@gmail.com>
2021-12-09 22:41:53 +0900

    Initial Commit

---
+++ WeSplitDay17/WeSplitDay17App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitDay17App.swift
+//  WeSplitDay17
+//
+//  Created by Mi Gyeong Park on 2021/12/06.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitDay17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
